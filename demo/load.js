a47c53acdfbe69d356550fa995ec952dd92e4033
vishaldaga <vishal.daga@hotelsoft.com>
2016-09-17 20:47:36 +0530

    only demo

--- demo/load.js
+++
@@ -1,6 +0,0 @@
-import ComboBoxWithInputDemo from "./Demo";
-import { render } from "react-dom";
-import React from "react";
-require("./styles.css");
-
-render(<ComboBoxWithInputDemo />, document.getElementById("content"));
