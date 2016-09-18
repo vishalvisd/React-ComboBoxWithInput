c58e460d8d42524e2dcc7a7ba47002dd6fab4353
vishaldaga <vishal.daga@hotelsoft.com>
2016-09-18 13:18:42 +0530

    corrected version for react peer dependency

--- demo/load.js
+++ demo/load.js
@@ -1,6 +1,5 @@
 import ComboBoxWithInputDemo from "./Demo";
-import { render } from "react-dom";
-import React from "react";
+import React, {render} from "react";
 require("./styles.css");
 
 render(<ComboBoxWithInputDemo />, document.getElementById("content"));
